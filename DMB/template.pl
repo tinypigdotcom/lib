4c5e386e870cd15f86fcbdfe198db39c9836bfc6
David Bradford <davembradford@gmail.com>
2014-06-22 19:01:08 -0400

    add perl template

---
+++ DMB/template.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use 5.14.0;
+use warnings;
+use autodie;
+
+use IO::File;
+
+my $ifh = IO::File->new($0, '<');
+die if (!defined $ifh);
+
+while(<$ifh>) {
+    chomp;
+    print "l: $_\n";
+}
+$ifh->close;
+
+my $ofh = IO::File->new('a.out', '>');
+die if (!defined $ofh);
+
+print $ofh "bar\n";
+$ofh->close;
+
